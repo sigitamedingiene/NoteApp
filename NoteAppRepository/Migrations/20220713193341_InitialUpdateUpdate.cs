7c345849514674d1f26b444b7e94565a5cec7d2e
Sigita Medingiene <sigitamedingiene@gmail.com>
2022-07-13 22:39:38 +0300

    Update Categorie entitie, add-migration and update data base.

---
+++ NoteAppRepository/Migrations/20220713193341_InitialUpdateUpdate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NoteAppRepository.Migrations
+{
+    public partial class InitialUpdateUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: true,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Categories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
